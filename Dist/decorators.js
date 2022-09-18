52a6a6cdd0e474349f5c3b929c36f148bd41346e
Stanislav <lazarev@yandex.ru>
2022-09-18 12:43:16 +0300

    start nameSpaces

--- Dist/decorators.js
+++ Dist/decorators.js
@@ -5,36 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Component(config) {
-    return function (Constructor) {
-        return class extends Constructor {
-            constructor(...args) {
-                super(...args);
-                const el = document.querySelector(config.selector);
-                el.innerHTML = config.template;
-            }
-        };
-    };
+const validators = {};
+function Required(target, propName) {
+    validators[target.constructor.name] = Object.assign(Object.assign({}, validators[target.constructor.name]), { [propName]: 'required' });
 }
-let CardComponent = class CardComponent {
-    constructor(name) {
-        this.name = name;
+function validate(obj) {
+    const objConfig = validators[obj.constructor.name];
+    if (!objConfig) {
+        return true;
     }
-    logName() {
-        console.log(`Component name ${this.name}`);
+    let isValid = true;
+    Object.keys(objConfig).forEach(key => {
+        if (objConfig[key] === 'required') {
+            isValid = isValid && !!obj[key];
+        }
+    });
+    return isValid;
+}
+class Form {
+    constructor(email) {
+        this.email = email;
     }
-};
-CardComponent = __decorate([
-    Component({
-        selector: '#card',
-        template: `
-    <div class="card">
-      <div class="card-content">
-        <span class="card-title">Card component</span>
-      </div>
-    </div>
-  `
-    })
-], CardComponent);
-const card = new CardComponent('My card component');
+}
+__decorate([
+    Required
+], Form.prototype, "email", void 0);
+const form = new Form('v@mail.ru');
+if (validate(form)) {
+    console.log('Valid', form);
+}
+else {
+    console.log('Validation Error');
+}
 //# sourceMappingURL=decorators.js.map
